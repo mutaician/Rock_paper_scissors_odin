697935f716fe34e4b65ea78ef5e7c3cb74ec6917
mutaician <109066669+mutaician@users.noreply.github.com>
2024-01-26 17:27:43 +0300

    added user interface

--- script.js
+++ script.js
@@ -1,37 +1,91 @@
 
+function start_game(){
+    player_score = 0
+    computer_score = 0
+    rock.addEventListener('click', rock_handler)
+    paper.addEventListener('click', paper_handler)
+    scissors.addEventListener('click', scissors_handler)
+    playerScore.textContent = 'You: '
+    computerScore.textContent = 'Computer: '
+    results.textContent = ''
+
+}
+
 function getComputerChoice(){
     const choices = ["Rock", "Paper", "Scissors"]
     let random_index = Math.floor(Math.random() * 3)
     return choices[random_index]
 }
 
-function playRound(playerSelection, computerSelection){
-    let player = playerSelection.toLowerCase()
-    let computer = computerSelection.toLowerCase()
+function checkChoices(player, computer){
     if (player == computer){
-        return "You Tie."
+        return "tie"
     }
-    else if (player == "rock" && computer == "scissors"){
-        return "You win! " + playerSelection + " beats " + computerSelection
+    else if ((player == "Rock" && computer == "Scissors") || 
+        (player == "Paper" && computer == "Rock") || 
+        (player == "Scissors" && computer == "Paper"))
+    return 'win'
+    
+    else return 'loose'
+}
+
+function calcResults(playerChoice) {
+    
+    let computerChoice = getComputerChoice()
+    let playResults = checkChoices(playerChoice, computerChoice)
+    if (playResults == 'win'){
+        results.textContent = "You win! " + playerChoice + " beats " + computerChoice
+        player_score ++
+        playerScore.textContent = "You: "+ player_score
     }
-    else if (player == "paper" && computer == "rock"){
-        return "You win! " + playerSelection + " beats " + computerSelection
-    }
-    else if (player == "scissors" && computer == " paper"){
-        return "You win! " + playerSelection + "beats " + computerSelection
+    else if (playResults == 'loose'){
+        results.textContent = "You loose! " + computerChoice + " beats " + playerChoice
+        computer_score ++
+        computerScore.textContent = "Computer: "+ computer_score
     }
     else {
-        return "You loose! " + computerSelection + " beats " + playerSelection
+        results.textContent = "You Tied"
+    }
+    if (player_score >= 5 || computer_score >= 5) {
+        decidewinner(player_score, computer_score)
+        rock_remover()
+        paper_remover()
+        scissors_remover()
+    }
+
+}
+
+function decidewinner(player_score, computer_score) {
+    if (player_score > computer_score) {
+        results.textContent = "WoW I Couldn't THink You'd WiN"
+    }
+    else {
+        results.textContent = "LuCKy For YOu, IT's JuSt RaNDom"
     }
 }
 
 
-function move(){
-    for (let i = 1; i <= 5; i++){
-        let playerSelection = prompt("What's your choice?")
-        let computerSelection = getComputerChoice()
-        console.log(playRound(playerSelection, computerSelection))
-    }
-}
+const rock_handler = function() {calcResults('Rock')}
+const paper_handler = function() {calcResults('Paper')}
+const scissors_handler = function() {calcResults('Scissors')}
+const rock_remover = function() {rock.removeEventListener('click', rock_handler)};
+const paper_remover = function() {paper.removeEventListener('click', paper_handler)}
+const scissors_remover = function() {scissors.removeEventListener('click', scissors_handler)}
 
-move()
+const rock = document.querySelector('.rock')
+const paper = document.querySelector('.paper')
+const scissors = document.querySelector('.scissors')
+const results = document.querySelector('.results')
+const divScore = document.querySelector('.score')
+const restart = document.querySelector('.restart')
+
+const playerScore = document.createElement('p')
+const computerScore = document.createElement('p')
+divScore.append(playerScore, computerScore)
+
+let player_score = 0
+let computer_score = 0
+
+
+restart.addEventListener('click', start_game)
+start_game()
