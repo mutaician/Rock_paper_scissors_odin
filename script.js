d59011c24322bdf7cd503745ac9687e2dc61d808
mutaician <109066669+mutaician@users.noreply.github.com>
2024-01-27 20:19:59 +0300

    addeed lizard and spock

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ function start_game(){
     rock.addEventListener('click', rock_handler)
     paper.addEventListener('click', paper_handler)
     scissors.addEventListener('click', scissors_handler)
+    lizard.addEventListener('click', lizard_handler)
+    spock.addEventListener('click', spock_handler)
     playerScore.textContent = 'You: '
     computerScore.textContent = 'Computer: '
     results.textContent = ''
@@ -12,34 +14,67 @@ function start_game(){
 }
 
 function getComputerChoice(){
-    const choices = ["Rock", "Paper", "Scissors"]
-    let random_index = Math.floor(Math.random() * 3)
+    const choices = ["Rock", "Paper", "Scissors", "Lizard", "Spock"]
+    let random_index = Math.floor(Math.random() * choices.length)
     return choices[random_index]
 }
 
-function checkChoices(player, computer){
-    if (player == computer){
-        return "tie"
-    }
-    else if ((player == "Rock" && computer == "Scissors") || 
-        (player == "Paper" && computer == "Rock") || 
-        (player == "Scissors" && computer == "Paper"))
-    return 'win'
-    
-    else return 'loose'
+function formartResults(firstChoice, secondChoice) {
+    let first = firstChoice.toLowerCase()
+    let second = secondChoice.toLowerCase()
+    const lookupTable = {
+      rock: {
+        scissors: "crushes",
+        lizard: "smashes",
+      },
+      paper: {
+        rock: "covers",
+        spock: "disproves",
+      },
+      scissors: {
+        paper: "cuts",
+        lizard: "decapitates",
+      },
+      lizard: {
+        spock: "poisons",
+        paper: "eats",
+      },
+      spock: {
+        rock: "vaporizes",
+        scissors: "smashes",
+      },
+    };
+    const result = lookupTable[first][second];
+    return `${firstChoice} ${result} ${secondChoice}`
+}
+
+const rules = {
+    Rock: ['Scissors', 'Lizard'],
+    Paper: ['Rock', 'Spock'],
+    Scissors: ['Paper', 'Lizard'],
+    Lizard: ['Paper', 'Spock'],
+    Spock: ['Rock', 'Scissors']
+  }
+
+function checkChoices(player, computer) {
+    if (player == computer) return 'tie'
+
+    if (rules[player].includes(computer)) return 'win'
+    if (rules[computer].includes(player)) return 'loose'
 }
 
 function calcResults(playerChoice) {
     
     let computerChoice = getComputerChoice()
+    console.log(playerChoice,computerChoice)
     let playResults = checkChoices(playerChoice, computerChoice)
     if (playResults == 'win'){
-        results.textContent = "You win! " + playerChoice + " beats " + computerChoice
+        results.textContent = `You win! ` + formartResults(playerChoice,computerChoice)
         player_score ++
         playerScore.textContent = "You: "+ player_score
     }
     else if (playResults == 'loose'){
-        results.textContent = "You loose! " + computerChoice + " beats " + playerChoice
+        results.textContent = "You loose! " + formartResults(computerChoice, playerChoice)
         computer_score ++
         computerScore.textContent = "Computer: "+ computer_score
     }
@@ -51,6 +86,8 @@ function calcResults(playerChoice) {
         rock_remover()
         paper_remover()
         scissors_remover()
+        lizard_remover()
+        spock_remover()
     }
 
 }
@@ -68,13 +105,19 @@ function decidewinner(player_score, computer_score) {
 const rock_handler = function() {calcResults('Rock')}
 const paper_handler = function() {calcResults('Paper')}
 const scissors_handler = function() {calcResults('Scissors')}
+const lizard_handler = function() {calcResults('Lizard')}
+const spock_handler = function() {calcResults('Spock')}
 const rock_remover = function() {rock.removeEventListener('click', rock_handler)};
 const paper_remover = function() {paper.removeEventListener('click', paper_handler)}
 const scissors_remover = function() {scissors.removeEventListener('click', scissors_handler)}
+const lizard_remover = function() {lizard.removeEventListener('click', lizard_handler)}
+const spock_remover = function() {spock.removeEventListener('click', lizard_handler)}
 
 const rock = document.querySelector('.rock')
 const paper = document.querySelector('.paper')
 const scissors = document.querySelector('.scissors')
+const lizard = document.querySelector('.lizard')
+const spock = document.querySelector('.spock')
 const results = document.querySelector('.results')
 const divScore = document.querySelector('.score')
 const restart = document.querySelector('.restart')
